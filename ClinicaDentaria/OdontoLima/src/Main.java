b2e5dfbd0be02a195cf19204a2dd24e5650b6031
Andre Luis Lima da Silva- Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2022-12-28 17:03:00 -0300

    creation of connect and packges

---
+++ ClinicaDentaria/OdontoLima/src/Main.java
@@ -0,0 +1,12 @@
+
+import services.Db;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Db c = new Db();
+		c.Connect();
+	}
+
+}
---
+++ ClinicaDentaria/OdontoLima/src/services/Db.java
@@ -0,0 +1,32 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Db {
+	
+	public static Connection Connect() {
+		
+		Connection conn = null;
+		
+		try {
+//			String host = "jdbc:mysql://127.0.0.1:3306/sys";
+//			String user = "root";
+//			String pass = "12345678";
+			
+			String host = "jdbc:mysql://db-step-ortolima-do-user-13131599-0.b.db.ondigitalocean.com:25060/dbClinica";
+			String user = "doadmin";
+			String pass = "AVNS_ojeKFczds4KsFKuPahQ";
+			
+			conn = DriverManager.getConnection(host,user,pass);
+			System.out.println("Conected!");
+			
+		}catch(SQLException e){
+			System.out.println(e);
+		}
+		
+		return conn;
+	}
+}
