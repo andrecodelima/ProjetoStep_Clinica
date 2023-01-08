a81d1dd03ad800ac53ec697d0853c2b0396f064a
andrecodelima <andrelimag89@gmail.com>
2023-01-08 17:44:15 -0300

    new project clinica odonto

---
+++ ClinicaDentaria/OdontoLima/src/Application/Main.java
@@ -0,0 +1,13 @@
+package Application;
+
+import services.Db;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Db c = new Db();
+		c.Connect();
+	}
+
+}
--- OdontoLima2/src/Main.java
+++ OdontoLima2/src/Main.java
@@ -1,12 +1,11 @@
-
 import services.Db;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Db c = new Db();
 		c.Connect();
+
 	}
 
 }
