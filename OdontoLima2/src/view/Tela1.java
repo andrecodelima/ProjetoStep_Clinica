8309d103beb6c019a8eaf9527ea71eb2d397e5e2
Andre Luis Lima da Silva - Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2023-02-03 14:19:31 -0300

    tela

--- OdontoLima2/src/view/Tela1.java
+++ OdontoLima2/src/view/Tela1.java
@@ -5,6 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import javax.swing.ImageIcon;
 
 public class Tela1 extends JFrame {
 
@@ -43,12 +49,17 @@ public class Tela1 extends JFrame {
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(0, 128, 192));
-		panel.setBounds(10, 11, 684, 364);
+		panel.setBackground(SystemColor.activeCaption);
+		panel.setBounds(0, 0, 704, 75);
 		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ODONTOLIMA");
+		lblNewLabel.setBounds(10, 11, 233, 64);
+		panel.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel.setForeground(SystemColor.control);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\andre.lima\\Documents\\GITHUB\\ProjetoStep_Clinica\\icons8-dental-care-64 (1).png"));
+		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 21));
 	}
-	/**
-	 * Create the application.
-	 */
-	
 }
