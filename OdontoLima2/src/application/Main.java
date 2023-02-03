46f0c35965b1b0f482104a3c37e283028515d221
Andre Luis Lima da Silva - Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2023-02-03 14:18:45 -0300

    GUI creation

--- OdontoLima2/src/application/Main.java
+++ OdontoLima2/src/application/Main.java
@@ -1,12 +1,18 @@
 package application;
 import view.Menu;
+import view.Tela1;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Menu m = new Menu();
-		m.menu();
+//		m.menu();
+		
+		Tela1 t = new Tela1();
+		t.setVisible(true);
+		
+		
 	}
 
 }
---
+++ OdontoLima2/src/view/Tela1.java
@@ -0,0 +1,54 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class Tela1 extends JFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tela1 window = new Tela1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+		
+	}
+	
+	public Tela1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 720, 425);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 128, 192));
+		panel.setBounds(10, 11, 684, 364);
+		frame.getContentPane().add(panel);
+	}
+	/**
+	 * Create the application.
+	 */
+	
+}
